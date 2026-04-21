cff4e7c4b07ecb94cd0910584b04516e6e30f151
boissieraleksandra-debug <boissieraleksandra@gmail.com>
2026-04-21 13:47:46 +0200

    Add startup dashboard with metrics and bar chart

--- pages/5_Startup_Dashboard.py
+++ pages/5_Startup_Dashboard.py
@@ -1,1 +1,18 @@
+import streamlit as st
+import pandas as pd
 
+st.title("🏢 Startup Dashboard")
+
+col1, col2, col3 = st.columns(3)
+
+col1.metric("Open Roles", 4)
+col2.metric("Applicants", 18)
+col3.metric("Avg Pay", "CHF 1600")
+
+data = pd.DataFrame({
+    "Job": ["Marketing", "Product", "Design"],
+    "Applicants": [8, 6, 4]
+})
+
+st.subheader("Applicants per Job")
+st.bar_chart(data.set_index("Job"))
