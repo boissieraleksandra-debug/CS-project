d034053b2fa31e0599f135ffb151b598aa22b12a
boissieraleksandra-debug <boissieraleksandra@gmail.com>
2026-04-21 13:47:11 +0200

    Add liked jobs display using Streamlit

--- pages/3_Liked_Jobs.py
+++ pages/3_Liked_Jobs.py
@@ -1,1 +1,11 @@
+import streamlit as st
 
+st.title("❤️ Liked Jobs")
+
+liked_jobs = [
+    "Marketing Intern at GrowthAI",
+    "Product Intern at TechFlow"
+]
+
+for job in liked_jobs:
+    st.write("✔️", job)
