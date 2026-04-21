a50e1785e644b8116d254456d89782880cb03136
boissieraleksandra-debug <boissieraleksandra@gmail.com>
2026-04-21 14:29:27 +0200

    Update 1_Profile.py

--- pages/1_Profile.py
+++ pages/1_Profile.py
@@ -1,1 +1,13 @@
+import streamlit as st
 
+st.title("👤 Profile")
+
+st.text_input("Full Name")
+st.text_input("LinkedIn")
+st.text_area("Education")
+st.text_area("Interests")
+st.text_input("Availability")
+
+st.file_uploader("Upload CV")
+
+st.button("Save Profile")
