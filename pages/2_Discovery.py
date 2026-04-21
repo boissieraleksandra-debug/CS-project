b7bbaf1a1931ebe32f768a4876854e4b901367dd
Faustine-code <claessensfaustine29@gmail.com>
2026-04-21 15:13:50 +0200

    Update 2_Discovery.py

--- pages/2_Discovery.py
+++ pages/2_Discovery.py
@@ -1,1 +1,9 @@
+import streamlit as st
 
+#with tells that everything that will be intented will have to be inside the box
+#st.container() is function call that returns an object. And we'll be able to include some buttons inside
+with st.container(border=True):
+    st.subheader("Sales Manager")
+    st.write("Location:", "Zurich, SUI")
+    st.write("Duration:", "3 months")
+    st.write("Tags:", "Sales, Revenues")
